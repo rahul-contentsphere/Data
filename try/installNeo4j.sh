f4bd6e81aa77096d97f2e50fcbee7bb7af47a2fc
rahul-contentsphere <rahul@contentsphere.com>
2017-05-31 05:07:47 +0530

    Delete installNeo4j.sh

--- try/installNeo4j.sh
+++
@@ -1,5 +0,0 @@
-cd /home/cs/allianz/base/
-echo "Downloading Neo4j....Wait."
-wget http://neo4j.com/artifact.php?name=neo4j-community-2.3.3-unix.tar.gz 
-tar -zxvf artifact.php?name=neo4j-community-2.3.3-unix.tar.gz
-mv neo4j-community-2.3.3 neo4j
